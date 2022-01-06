3088c1ca115483ddb028f6f8d0026d89775ec2a1
Adrian Golian <adix9512@gmail.com>
2022-01-06 02:03:06 +0100

    Refactor JSON typealias

--- Blef/ErrorResponse.swift
+++ Blef/ErrorResponse.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 
-typealias JSON = [String: Any]
-
 struct ErrorResponse {
     var error: String
 }
---
+++ Blef/JSON.swift
@@ -0,0 +1,13 @@
+//
+//  JSON.swift
+//  Blef
+//
+//  Created by Adrian Golian on 05/01/2022.
+//  Copyright © 2022 Blef. All rights reserved.
+//
+
+import Foundation
+
+typealias JSON = [String: Any]
+
+typealias JSONArray = [JSON]
