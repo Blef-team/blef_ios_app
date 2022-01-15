59afd3785c28be4a6592f267eef98e8e1a9aa9a6
Adrian Golian <adix9512@gmail.com>
2022-01-15 15:55:09 +0100

    Add room sprites and labels to Join scene

--- Blef/JoinScene.swift
+++ Blef/JoinScene.swift
@@ -16,6 +16,8 @@ class JoinScene: SKScene {
     var gameManager: GameManager?
     var messageLabel: SKLabelNode!
     var isDisplayingMessage = false
+    private var roomSprites: [SKSpriteNode] = []
+    private var roomLabels: [SKLabelNode] = []
     
     override func didMove(to view: SKView) {
         
@@ -28,5 +30,29 @@ class JoinScene: SKScene {
         messageLabel.preferredMaxLayoutWidth = size.width * 0.8
         messageLabel.verticalAlignmentMode = .center
         self.addChild(messageLabel)
+        
+        
+        roomSprites = []
+        for roomIndex in 0...17 {
+            let position = getRoomPosition(roomIndex)
+            let sprite = SKSpriteNode(texture: SKTexture(image: #imageLiteral(resourceName: "white-door")), size: CGSize(width: 120, height: 120))
+            sprite.alpha = 0
+            sprite.position = position
+            addChild(sprite)
+            roomSprites.append(sprite)
+            let label = SKLabelNode(fontNamed:"HelveticaNeue-Light")
+            label.fontSize = 30
+            label.alpha = 0
+            label.position = position
+            addChild(label)
+            roomLabels.append(label)
+        }
     }
+    
+    func getRoomPosition(_ roomIndex: Int) -> CGPoint {
+        let xPosition = size.width * -0.35 + CGFloat(100 * (roomIndex % 6))
+        let yPosition = size.height * -0.3 * CGFloat((Double(roomIndex / 6) - 0.7).rounded())
+        return CGPoint(x: xPosition, y: yPosition)
+    }
+    
 }
